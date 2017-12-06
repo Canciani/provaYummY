e70ceb5dea9555ebf7f7a7066fd0055b065ff5d9
Andrea Carminati <carminatiandrea94@gmail.com>
2017-12-06 11:45:27 +0100

    prima prova di connessione con dialog flow.

--- src/chat.js
+++ src/chat.js
@@ -1,104 +1,71 @@
 class Chat extends HTMLElement {
   constructor() {
         super();
-        //Once templates initialized start API.ai interaction
-        const promise = app.apiaiClient.eventRequest("Ciao Bimbo");
-        //The element instance is bound to the promise
+
+        const promise = webApp.apiaiClient.textRequest("Il cane abbaia")
+
         promise
             .then(this.handleResponse.bind(this))
             .catch(this.handleError);
   }
 
+  //Funzione che aggiunge alla chat il messaggio vreato dalla serverResponse
   handleResponse(serverResponse) {
-
-        //console.log("--> handleResponse");
-        // console.log(serverResponse);
-        // console.log(serverResponse.result.fulfillment.messages);
-        this.appendMessages(serverResponse.result.fulfillment.messages)
-        // var resp = JSON.parse(response);
-        // console.log(resp);
+    //serverResponse.result.fulfillment.messages entra nel JSON
+    this.appendYummyMessages(serverResponse.result.fulfillment.messages);
   }
 
+  //funzione che manda gli errori al console.log
   handleError(serverError) {
       console.log(serverError);
   }
 
-  appendMessages(messages) {
-      /* NOTE: DEN
-      console.log(messages);
-      // console.log(x);
-      // console.log(messages[0].speech);
-
-      // Get the link element that references the templates.html file.
-      var templateLink = document.getElementById('template-conversation');
-      // Retrieve the loaded templates.
-      var template = templateLink.import;
-      this.elementBot = template.getElementById('conversation-bot');
-      this.elementUser = template.getElementById('conversation-user');
-      console.log(this);
-      console.log(this.elementBot);
-
-      for (let i = 0; i < messages.length; i++) {
-          let msg = messages[i];
-          switch (msg.type) {
-              case 0:
-                  var cloneBot = document.importNode(this.elementBot.content, true);
-                  var dialog = document.createElement("p");
-                  // console.log(msg.speech);
-                  dialog.textContent = msg.speech;
-                  cloneBot.querySelector('#bot').appendChild(dialog);
-                  this.appendChild(cloneBot);
-                  break;
-              case 4:
-                  //Need a map that holds the type of the req, the type of speaker (bot or user) and the possible elements to render
-                  var cloneUser = document.importNode(this.elementUser.content, true);
-                  var contentElement = msg.payload.element;
-                  var buttons = msg.payload.buttons;
-                  console.log(buttons);
-
-                  for (let j = 0; j < buttons.length; j++) {
-                      let dialog = document.createElement(contentElement);
-                      dialog.textContent = buttons[j].text;
-                      dialog.addEventListener('click', this.buttonListener.bind(this, dialog));
-                      cloneUser.querySelector('#user').appendChild(dialog);
-                  }
-                  this.appendChild(cloneUser);
-
-                  break;
-              default:
-                  console.log("unknown message type");
-          }
-      }
-      */
+  appendChildMessages(messages) {
+      //creo html
+      var childrow  = document.createElement("div");
+      childrow.setAttribute("class", "rowdx");
+      //creo il messaggio del bimbo
+      var childmsg  = document.createElement("div");
+      childmsg.setAttribute("class", "childchat");
+      //creo il testo
+      var childtext  = document.createElement("p");
+      childtext.setAttribute("class", "paddingtesto");
+      //append
+      childtext.innerHTML = messages;
+      childmsg.appendChild(childtext);
+      childrow.appendChild(childmsg);
+      this.$.display.appendChild(childrow);
   }
 
+  appendYummyMessages(message) {
+      //manda il messaggio al log
+      console.log(message);
+      //creo html
+      //creo la riga yummy;
+      var yummyrow  = document.createElement("div");
+      yummyrow.setAttribute("class", "rowsx");
+      //creo il messaggio del bimbo
+      var yummymsg  = document.createElement("div");
+      yummymsg.setAttribute("class", "yummychat");
+      var yummytext  = document.createElement("p");
+      yummytext.setAttribute("class", "paddingtesto");*/
 
-    homeRequest(text) {
-        var requestOutput = app.apiaiClient.textRequest(text);
-        console.log(requestOutput);
-    }
+      var rispostaYummy = message;
 
-    //da vedere a cosa attacca il listener: con handle handleTap
-    buttonListener(dialog, cloneUser) {
-        console.log(dialog);
-        console.log(this);
+      //append
+      yummytext.innerHTML = rispostaYummy;
+      yummymsg.appendChild(yummytext);
+      yummyrow.appendChild(yummymsg);
+      this.$.display.appendChild(yummyrow);
+  }
 
-        console.log(dialog.parentNode);
+  childRequest(text) {
+    const promise = webApp.apiaiClient.textRequest(text);
 
-        var query = dialog.textContent;
-
-        //Set the button as response
-        dialog.parentNode.innerHTML = query;
-
-        //Trigger API.ai interaction
-        const promise = app.apiaiClient.textRequest(query);
-        // //The element instance is bound to the promise
-        promise
-            .then(this.handleResponse.bind(this))
-            .catch(this.handleError);
-
-    }
-
+    promise
+        .then(this.handleResponse.bind(this))
+        .catch(this.handleError);
+  }
 }
 
 customElements.define('chat', Chat);
